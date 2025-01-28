342efcb6a4dada806ad461cf8421d1d802550e16
GyuJae <wjdrbwo1206@naver.com>
2025-01-28 14:47:59 +0900

    Reader-Writer Lock

--- ServerCore/CorePch.h
+++ ServerCore/CorePch.h
@@ -14,7 +14,8 @@
 #include <unordered_map>
 #include <unordered_set>
 
+#include <windows.h>
 #include <iostream>
 using namespace std;
 
-void HellowWorld();
+#include "Lock.h"
---
+++ ServerCore/Lock.cpp
@@ -0,0 +1,90 @@
+#include "pch.h"
+#include "Lock.h"
+#include "CoreTLS.h"
+
+void Lock::WriteLock()
+{
+	const uint32 lockThreadId = (_lockFlag.load() & WRITE_THREAD_MASK) >> 16;
+	if(LThreadId == lockThreadId)
+	{
+		_writeCount++;
+		return;
+	}
+
+	const int64 beginTick = ::GetTickCount64();
+	const uint32 desired = ((LThreadId << 16) & WRITE_THREAD_MASK);
+	while (true)
+	{
+		for (uint32 spinCount = 0; spinCount < MAX_SPIN_COUNT; ++spinCount)
+		{
+			uint32 expected = EMPTY_FLAG;
+			if (_lockFlag.compare_exchange_strong(OUT expected, desired))
+			{
+				_writeCount++;
+				return;
+			}
+		}
+
+		if (::GetTickCount64() - beginTick > ACQUIRE_TIMEOUT_TICK)
+		{
+			CRASH("LOCK_TIMEOUT");
+		}
+
+		this_thread::yield();
+	}
+}
+
+void Lock::WriteUnlock()
+{
+	// ReadLock 다 풀기 전에는 WriteLock을 풀 수 없다.
+	if ((_lockFlag.load() & READ_COUNT_MASK) != 0)
+	{
+		CRASH("INVALID_UNLOCK_ERROR");
+	}
+
+	const int32 writeCount = --_writeCount;
+	if (writeCount == 0)
+	{
+		_lockFlag.store(EMPTY_FLAG);
+	}
+}
+
+void Lock::ReadLock()
+{
+	// 동일한 쓰레드가 소유하고 있다면 무조건 성공
+	const uint32 lockThreadId = (_lockFlag.load() & WRITE_THREAD_MASK) >> 16;
+	if (LThreadId == lockThreadId)
+	{
+		_lockFlag.fetch_add(1);
+		return;
+	}
+	
+	const int64 beginTick = ::GetTickCount64();
+	while (true)
+	{
+		for (uint32 spinCount = 0; spinCount < MAX_SPIN_COUNT; ++spinCount)
+		{
+			uint32 expected = (_lockFlag.load() & READ_COUNT_MASK);
+			uint32 desired = expected + 1;
+			if (_lockFlag.compare_exchange_strong(OUT expected, desired))
+			{
+				return;
+			}
+		}
+
+		if (::GetTickCount64() - beginTick > ACQUIRE_TIMEOUT_TICK)
+		{
+			CRASH("LOCK_TIMEOUT");
+		}
+
+		this_thread::yield();
+	}
+}
+
+void Lock::ReadUnlock()
+{
+	if ((_lockFlag.fetch_sub(1) & READ_COUNT_MASK) == 0)
+	{
+		CRASH("MULTIPLE_UNLOCK");
+	}
+}
